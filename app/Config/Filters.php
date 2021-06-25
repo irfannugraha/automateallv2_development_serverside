3554a967e472a4870493bcbf9fc9997a43732737
sahong24 <agungpadhika@gmail.com>
2021-06-25 21:23:13 +0700

    add filters

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -33,7 +33,7 @@ class Filters extends BaseConfig
 		'before' => [
 			// 'honeypot',
 			// 'csrf',
-			'jwtauth' => ['except' => ['/', '/auth/*']],
+			'jwtauth' => ['except' => ['/', '/auth/*', '/blog', '/blog/*']],
 		],
 		'after'  => [
 			'toolbar',
